8612ae5a1a4c33ee5bc0eb1705cd0e952f5802f8
matt-in-a-hat <matt@mediasuite.co.nz>
2015-03-17 10:09:05 +1300

    Add support for array or string input and output

--- lib/Overpass2Geojson.php
+++ lib/Overpass2Geojson.php
@@ -2,16 +2,40 @@
 
 class Overpass2Geojson
 {
-
+    /**
+     * Converts a JSON string or decoded array into a GeoJSON string
+     * @param  mixed $input JSON string or array
+     * @return string       GeoJSON string
+     */
     public static function convert($input) {
-        if (!is_string($input)) {
-            return false;
-        }
-        $inputArray = json_decode($input, true);
-        if (!isset($inputArray['elements']) || !is_array($inputArray['elements'])) {
+        if (is_array($input)) {
+            $inputArray = $input;
+        } else if (is_string($input)) {
+            $inputArray = json_decode($input, true);
+        } else {
             return false;
         }
 
-        return '';
+        $output = self::convertArray($inputArray);
+        return $output !== false ? json_encode($output) : $output;
+    }
+
+    /**
+     * Converts a JSON string or decoded array into a GeoJSON array
+     * @param  mixed $input JSON string or array
+     * @return array       Converted to GeoJSON structure
+     */
+    public static function convertArray($input) {
+        if (is_string($input)) {
+            $input = json_decode($input, true);
+        }
+        if (!is_array($input) ||
+            !isset($input['elements']) ||
+            !is_array($input['elements'])) {
+
+            return false;
+        }
+
+        return array();
     }
 }
--- test/Overpass2GeojsonTest.php
+++ test/Overpass2GeojsonTest.php
@@ -2,7 +2,7 @@
 class Overpass2GeojsonTest extends PHPUnit_Framework_TestCase
 {
 
-    public function testInputValidation() {
+    public function testConvertInputValidation() {
 
         $input = null;
         $output = Overpass2Geojson::convert($input);
@@ -18,10 +18,49 @@ class Overpass2GeojsonTest extends PHPUnit_Framework_TestCase
 
         $input = '{}';
         $output = Overpass2Geojson::convert($input);
-        $this->assertSame(false, $output, 'Should return false when given an object without elements');
+        $this->assertSame(false, $output, 'Should return false when given json without elements');
+
+        $input = array();
+        $output = Overpass2Geojson::convert($input);
+        $this->assertSame(false, $output, 'Should return false when given an array without elements');
 
         $input = '{ "elements": [] }';
         $output = Overpass2Geojson::convert($input);
-        $this->assertTrue(is_string($output), 'Should return a string when given a valid object');
+        $this->assertTrue(is_string($output), 'Should return a string when given valid json');
+
+        $input = array('elements' => array());
+        $output = Overpass2Geojson::convert($input);
+        $this->assertTrue(is_string($output), 'Should return a string when given a valid array');
+    }
+
+    public function testConvertArrayInputValidation() {
+
+        $input = null;
+        $output = Overpass2Geojson::convertArray($input);
+        $this->assertSame(false, $output, 'Should return false when given null');
+
+        $input = 42;
+        $output = Overpass2Geojson::convertArray($input);
+        $this->assertSame(false, $output, 'Should return false when given something not a string');
+
+        $input = '';
+        $output = Overpass2Geojson::convertArray($input);
+        $this->assertSame(false, $output, 'Should return false when given an empty string');
+
+        $input = '{}';
+        $output = Overpass2Geojson::convertArray($input);
+        $this->assertSame(false, $output, 'Should return false when given json without elements');
+
+        $input = array();
+        $output = Overpass2Geojson::convertArray($input);
+        $this->assertSame(false, $output, 'Should return false when given an array without elements');
+
+        $input = '{ "elements": [] }';
+        $output = Overpass2Geojson::convertArray($input);
+        $this->assertTrue(is_array($output), 'Should return an array when given valid json');
+
+        $input = array('elements' => array());
+        $output = Overpass2Geojson::convertArray($input);
+        $this->assertTrue(is_array($output), 'Should return an array when given a valid array');
     }
 }
